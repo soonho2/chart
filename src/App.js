75beccc9d4a00af83cf56efaf46e46d3096c1b67
Soonho Kim <soonho.kim@cgiar.org>
2022-10-27 15:05:19 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
+function App() 
+    {
+    return 
+        (
+        <div>
+            <LineChart />
+            <BarChart />
+            <PieChart />
+         </div>
+         );
+    }
 export default App;
